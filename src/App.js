13bfc3660c56a1c3aa17c90f7316ebdb3d7c70f9
Pete Gilbert <pete.gilbert16@gmail.com>
2020-06-25 17:04:49 -0400

    added firebase.js and removed it from app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
 import React, { Component } from 'react';
 import './index.css';
-import firebase from 'firebase/app';
-import 'firebase/database';
-const firebaseConfig = {
-  apiKey: "AIzaSyBvJwO2yQSGErN9NBZQnldxiKSwQSt1mkc",
-  authDomain: "quarantine-pursuit.firebaseapp.com",
-  databaseURL: "https://quarantine-pursuit.firebaseio.com",
-  projectId: "quarantine-pursuit",
-  storageBucket: "quarantine-pursuit.appspot.com",
-  messagingSenderId: "800505781023",
-  appId: "1:800505781023:web:445c432c49eb4bf083ae2a"
-};
-firebase.initializeApp(firebaseConfig);
-export default firebase;
 
 class App extends Component {
   render() {
