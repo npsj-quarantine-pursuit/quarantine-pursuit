82e5279c6f55f59d29e28a6e88aa1482f0893787
Pete Gilbert <pete.gilbert16@gmail.com>
2020-06-26 15:32:31 -0400

    Built initial Router Structure, started CreatQuiz Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import './index.scss';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import CreateQuiz from './CreateQuiz.js'
+import SelectQuiz from './SelectQuiz.js'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activeQuizPath: ""
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h1>Quarantine Pursuit</h1>
-      </div>
+      <Router>
+        <div className="app">
+          <header>
+            <h1>Quarantine Pursuit</h1>
+          </header>
+          <main>
+            <Link className="button" to="/create">Create a Quiz!</Link>
+            {/* <Link className="button" to="/select">Select an Existing Quiz!</Link> */}
+            <Route path="/create" component={CreateQuiz} />
+            {/* <Route path="/select" component={SelectQuiz} /> */}
+          </main>
+        </div>
+      </Router>
     )
   }
 }
