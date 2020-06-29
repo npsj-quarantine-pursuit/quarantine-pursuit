5f8da5ead0d15e0f47584b3dd07d48318c3c0cdc
nigelrudolf <mansell.nigel@mac.com>
2020-06-29 12:37:23 -0400

    refactored question display in PlayQuiz

--- src/Components/PlayQuiz.js
+++ src/Components/PlayQuiz.js
@@ -7,7 +7,7 @@ class PlayQuiz extends Component {
     this.state = {
       currentQuestion: {
         question: ' ',
-        incorrect_answers: ' ',
+        incorrect_answers: [],
       },
       questionNumber: 0,
       position: 1,
@@ -59,30 +59,34 @@ class PlayQuiz extends Component {
 
 
   setCorrectPosition = () => {
-    let correctPosition = Math.floor((Math.random() * 4) + 1);
+    let correctPosition = Math.floor(Math.random() * 4);
     this.setState({ 
       position: correctPosition
     })
   }
 
   render() {
+    let incorrectAnswers = this.state.currentQuestion.incorrect_answers;
+    // Because array is only 3 objects long, the || statement prints it if the correct answer is in 4th position
+    let listIncorrectAnswers = incorrectAnswers.map((answer, i) => {
+      if (this.state.position === i || (i === 2 && this.state.position == 4)) {
+        return (
+          <div>
+            <p>{this.state.currentQuestion.correct_answer}</p>  
+            <button onClick={this.handleClick}>{answer}</button>
+          </div>
+        );
+      } else { 
+      return <button onClick={this.handleClick}>{answer}</button>
+      }
+    });
+     
 
     return (
-
       <div>
         { this.state.showFinalScore ? <ShowFinalScore quiz={this.props.quiz} score={this.state.score}/> : null }
         <h2>{this.state.currentQuestion.question}</h2>
-
-        {this.state.position == 1 ? <button onClick={this.correct}>{this.state.currentQuestion.correct_answer}</button> : null}
-        <button onClick={this.handleClick}>{this.state.currentQuestion.incorrect_answers[0]}</button>
-
-        {this.state.position == 2 ? <button onClick={this.correct}>{this.state.currentQuestion.correct_answer}</button> : null}
-        <button onClick={this.handleClick}>{this.state.currentQuestion.incorrect_answers[1]}</button>
-
-        {this.state.position == 3 ? <button onClick={this.correct}>{this.state.currentQuestion.correct_answer}</button> : null}
-        <button onClick={this.handleClick}>{this.state.currentQuestion.incorrect_answers[2]}</button>
-
-        {this.state.position == 4 ? <button onClick={this.correct}>{this.state.currentQuestion.correct_answer}</button> : null}
+        {listIncorrectAnswers}
       </div>
     )
   }
